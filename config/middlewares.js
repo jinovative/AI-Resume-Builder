e78559f1b5f3169bf2d24b9df794452dca6aa7e2
Jin <jinyung7428@gmail.com>
2025-02-05 00:22:04 -0500

    add cors for specific link

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
+    "strapi::cors",
+    {
+        name: "strapi::cors",
+        config: {
+            origin: "https://ai-resume-builder-front-hmftplpyi-jinovatives-projects.vercel.app", // Frontend origin
+            methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+            headers: "*",
+        },
+    },
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
